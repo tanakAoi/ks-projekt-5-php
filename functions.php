c46ac20627bb5982331083df7ee4429a38297159
Aoi Tanaka <aoi.tanaka@studerande.karlskogafhs.se>
2023-03-20 04:11:59 +0900

    Add menu

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * Register Navigation Locations
+ * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
+ * 
+ * Register our locations for our header and sidebar menu.
+ * This will add a link to Menus under Appearance.
+ */
+
+ if ( ! function_exists( 'bdocs_register_nav_menu' ) ) {
+
+	function bdocs_register_nav_menu(){
+		register_nav_menus( array(
+	    	'header_menu' => __( 'Header Menu', 'bdocs_domain' ),
+	    	'siderbar_menu'  => __( 'Siderbar Menu', 'bdocs_domain' ),
+		) );
+	}
+	add_action( 'after_setup_theme', 'bdocs_register_nav_menu', 0 );
+}
