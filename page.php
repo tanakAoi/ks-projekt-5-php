56d2393a615c96101e1d716c4a53cae759bb3345
Aoi Tanaka <aoi.tanaka@studerande.karlskogafhs.se>
2023-03-27 02:12:08 +0900

    Get dynamic page content

--- page.php
+++ page.php
@@ -6,54 +6,29 @@
 
                 <?php get_sidebar(); ?>
 
-                <!-- Article -->
-                <article class="page">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-                    <header class="page__header">
+                    <!-- Article -->
+                    <article class="page">
 
-                    <?php get_template_part('breadcrumb') ?>
+                        <header class="page__header">
+                        <?php get_template_part('breadcrumb') ?>
 
-                        <h1>Docs Theme</h1>
-                    </header>
+                        <h1><?php the_title(); ?></h1>
+                        </header>
 
-                    <p>Lorem ipsum dolor sit amet. Ut deleniti fugaAut voluptas At doloribus odio. Aut ipsum reprehenderit et dolores dolor <a href="https://www.loremipzum.com" target="_blank">Qui cumque eum aliquid rerum est enim aspernatur</a>. </p>
+                        <!-- Page Content -->
+                        <?php the_content(); ?>
 
-                    <h2 id="quick-start">Quick Start from Template</h2>
+                        <footer class="page__footer">
+                            <div class="page__lastupdated">Last updated on <?= get_post_modified_time( 'j F, Y' ); // 8 Januari, 2023 ?></div>
 
-                    <h3 id="deploy-to">Deploy to Vercel</h3>
+                            <?php get_template_part('pagination') ?>
+                        </footer>
+                            
+                    </article>
 
-                    <p>Ut accusamus evenietaut tenetur et quae asperiores? Ut labore velit sed inventore voluptatem <a href="https://www.loremipzum.com" target="_blank">In autem et repellendus commodi</a> et nihil sunt sit neque odio ut quia cupiditate. Id expedita veritatis sit nostrum quiaUt nihil sed unde totam non ratione dolorem hic repellat esse. </p>
-
-                    <h3 id="fork-the">Fork the Template</h3>
-
-                    <p>In adipisci eveniet et dolores nobisA illum in minus quod ex ipsa quia id aliquid omnis ea molestias nobis. Qui provident libero sit rerum undeEst quas et dolorem saepe ex quas cumque eum libero culpa non consequatur facilis. Eos accusamus debitis <a href="https://www.loremipzum.com" target="_blank">Aut assumenda et similique maxime ab mollitia delectus</a> ut consequuntur corporis id odio temporibus vel eligendi consequatur. </p>
-
-                    <h2 id="start-as">Start as New Project</h2>
-
-                    <p>Non laudantium sunt quo natus erroret temporibus eum laudantium iure est nostrum consectetur. Ut nulla voluptasEum dolorem et labore possimus ad nesciunt quos! In numquam doloribusin aperiam non possimus quibusdam! </p>
-
-                    <h3 id="install">Install</h3>
-
-                    <p>Eos internos soluta <a href="https://www.loremipzum.com" target="_blank">Aut consequatur</a> vel inventore ducimus. Aut tempore minimaQui deleniti id atque natus non expedita nesciunt eos perferendis cupiditate est voluptas dolore et voluptatem dolores. </p>
-
-                    <h3 id="add-next-js">Add Next.js Config</h3>
-
-                    <p>Et earum voluptas est voluptatem nostrumAb voluptates ab quas magnam aut odio mollitia hic quidem facilis qui nisi sequi. Sed voluptatem perferendis qui quia sequi <a href="https://www.loremipzum.com" target="_blank">Eum placeat sed voluptatem tenetur et exercitationem reiciendis est autem odio</a>. </p>
-
-                    <h3 id="create-docs">Create Docs Theme Config</h3>
-
-                    <p>Rem nobis deseruntEt tempore ut iusto recusandae ut odio similique eum galisum impedit! Et laboriosam impeditSed consequatur ab eaque vero qui omnis iure sed illum odio. Et dolorem rerum <a href="https://www.loremipzum.com" target="_blank">Id saepe vel soluta accusamus hic eveniet magnam et laboriosam temporibus</a>. Eos excepturi errorUt eveniet qui neque quia ut aliquid quasi. </p>
-
-                    <h3 id="ready-to">Ready to Go!</h3>
-
-                    <p>In quos fuga <a href="https://www.loremipzum.com" target="_blank">Ad voluptate et tenetur dolorem est voluptatem aliquam est quaerat labore</a> ut rerum quia sit sint itaque! Est ratione sunt cum fugiat impeditEa corporis. Et vitae internos et omnis molestiashic possimus. Id omnis quibusdamEt labore non voluptatem debitis et pariatur voluptatem ut rerum officia id saepe culpa est quidem pariatur. </p>
-
-                    <footer class="page__footer">
-                        <div class="page__lastupdated">Last updated on February 20, 2023</div>
-                        <?php get_template_part('pagination') ?>
-                    </footer>
-
-                </article>
+                <?php endwhile; endif; ?>
 
                 <?php get_template_part('toc') ?>
 
